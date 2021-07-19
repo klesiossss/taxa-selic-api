ef6c7e7a5e521db61d15df63c3bf4409cbbbaeda
M252032 <klesio.s@tcs.com>
2021-07-19 09:59:28 -0300

    adicionando teste de consulta a taxa selic

--- src/test/java/br/com/taxaselicapi/service/ServiceTest.java
+++ src/test/java/br/com/taxaselicapi/service/ServiceTest.java
@@ -2,6 +2,7 @@ package br.com.taxaselicapi.service;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +18,7 @@ import br.com.taxaselicapi.model.Parcela;
 import br.com.taxaselicapi.model.Produto;
 import br.com.taxaselicapi.model.Request;
 import br.com.taxaselicapi.model.Response;
+import br.com.taxaselicapi.model.dto.SelicResponse;
 
 
 
@@ -34,6 +36,7 @@ public class ServiceTest {
 	private static List<Parcela> parcelas;
 	private static Request request;
 	private static Response response;
+	private static List<SelicResponse> selicResp;
 	
 	
 	@BeforeAll
@@ -51,6 +54,8 @@ public class ServiceTest {
 	
 		request = new Request();
 		response = new Response();
+		
+		selicResp = new ArrayList<>();
 	
 	}
 	
@@ -89,12 +94,25 @@ public class ServiceTest {
 }
 	
 	
-//	@Test
-//	@DisplayName("Deve lancar a excecao caso o stock nao reflita com a api")
-//	void testLancaExcessao() {
-//		when(quotedStockRepo.findById(1L)).thenReturn(Optional.empty());		
-//		assertThrows(BusinessException.class, () -> quotedStockService.salvar(quotedStock));
-//	}
+	@Test
+	@DisplayName("Deve consultar as taxas de juros dos ultimos 30 dias")
+	void testConsultaTaxaSelic() {
+	//	when(produtoService.ConsultaUltimasTaxasSelic()).thenReturn(selicResp);	
+		
+		List<SelicResponse> consulta = produtoService.ConsultaUltimasTaxasSelic();
+		selicResp = produtoService.ConsultaUltimasTaxasSelic();
+		
+		assertAll(() -> {
+		for(int i =0; i< selicResp.size(); i++) {
+			
+			assertEquals(selicResp,selicResp);
+			assertEquals(consulta.get(i).getData(),selicResp.get(i).getData());
+			assertEquals(consulta.get(i).getValor(),selicResp.get(i).getValor());
+			
+			}
+		});
+		
+	}
 
 	
 }
