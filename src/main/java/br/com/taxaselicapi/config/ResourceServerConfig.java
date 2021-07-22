5ec141e8c4feb9cc930244befe7f921ff7aceea5
M252032 <klesio.s@tcs.com>
2021-07-22 18:12:16 -0300

    adding auth2

--- src/main/java/br/com/taxaselicapi/config/ResourceServerConfig.java
+++ src/main/java/br/com/taxaselicapi/config/ResourceServerConfig.java
@@ -1,16 +1,22 @@
 package br.com.taxaselicapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @EnableWebSecurity
-public class ResourceServerConfig extends WebSecurityConfigurerAdapter{
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Autowired
+	public  void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
 		.withUser("admin").password("{noop}admin").roles("ROLE");
 	}
@@ -18,14 +24,21 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter{
 	
 	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/compra/taxaSelic").permitAll()
 		.anyRequest().authenticated()
 		.and()
-		.httpBasic().and()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 		.csrf().disable();
 	}
+	
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.stateless(true);
+	}
+
+	
+	
+	
 
 }
